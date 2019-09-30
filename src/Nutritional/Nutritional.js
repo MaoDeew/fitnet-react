af589f93b0a4611413b94fad2e398ce280d8da02
MaoDeew <deewweb@gmail.com>
2019-09-30 00:36:45 -0500

    Creating the first version of the Nutritional page. One change is from index.html adding Font Awesome. Also applying the axios instance specifically with only ingredients.

--- src/Nutritional/Nutritional.js
+++ src/Nutritional/Nutritional.js
@@ -1,19 +1,134 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-class Nutritional extends Component{
+import Fab from '@material-ui/core/Fab';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 
+import axios from './axiosInstance';
+import Ingredient from './Ingredient';
 
-    render(){
-        return(
+class Nutritional extends Component {
+
+    state = {
+        ingredients: [],
+        loading: false,
+        nextPage: null,
+        previousPage: null
+    }
+
+    componentDidMount() {
+        this.setState({
+            loading: true
+        });
+
+        axios.get("/ingredient/?language=2&status=2")
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    ingredients: response.data.results,
+                    loading: false,
+                    nextPage: response.data.next,
+                    previousPage: response.data.previous
+                });
+            })
+            
+    }
+
+    render() {
+        return (
             <div>
-            <NavBar />
-            <div>
-            <h1>This is nutritional</h1>
-            </div>
+                <NavBar />
+                <h1 style={{ textAlign: 'center' }}>Pick one of the ingredients</h1>
+
+                <Grid style={{ flexGrow: 1 }} container spacing={3}>
+                    <Grid style={{ display: 'inline-block' }} item sm={7}>
+
+                       {this.state.loading ? this.renderLoading() : this.renderIngredients()}
+
+                    </Grid>
+                    <Grid style={{ display: 'inline-block' }} item sm={5}>
+                        <h2>Count of calories</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </Grid>
+                </Grid>
+
             </div>
         );
     }
+
+    renderLoading() {
+        return (
+            <div>
+                <h1 style={{ display: 'inline-block', paddingRight: 24 }}>Loading Ingredients</h1> <CircularProgress />
+            </div>
+        );
+    }
+
+    renderPagination(){
+        return(
+            <div style={{ display: 'inline-block'}}>
+            
+           <Fab size='small' onClick={this.handleClickPrevious} disabled={this.state.previousPage==null ? true: false} color="primary" aria-label="add" >
+                <SkipPreviousIcon />
+            </Fab>
+            <Fab size='small' onClick={this.handleClickNext} disabled={this.state.nextPage==null ? true: false} color="primary" aria-label="add">
+                <SkipNextIcon />
+            </Fab>
+        </div>
+        );
+    }
+
+    renderIngredients(){
+        return(
+            <div>
+            <h1 style={{ display: 'inline-block', paddingRight: 24 }}>Ingredients</h1>
+            {this.renderPagination()}
+        <div>
+            {this.state.ingredients.map(ingredient => <Ingredient 
+            key={ingredient.id} name={ingredient.name} 
+            calories={ingredient.energy} 
+            protein={ingredient.protein} 
+            carbohydrates={ingredient.carbohydrates} 
+            fats={ingredient.fat}/>)}
+            </div>
+        </div>
+        );
+      
+    }
+
+    handleClickNext = () => {
+        axios.get(this.state.nextPage)
+            .then(response => {
+                
+                this.setState({
+                    ingredients: response.data.results,
+                    loading: false,
+                    nextPage: response.data.next,
+                    previousPage: response.data.previous
+                });
+            })
+      }
+
+      handleClickPrevious = () => {
+        axios.get(this.state.previousPage)
+            .then(response => {
+                
+                this.setState({
+                    ingredients: response.data.results,
+                    loading: false,
+                    nextPage: response.data.next,
+                    previousPage: response.data.previous
+                });
+            })
+      } 
 
 }
 
