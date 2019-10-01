dd343d1467a475c4962be9e5a93bb2b3641fc7d3
MaoDeew <deewweb@gmail.com>
2019-09-30 23:38:29 -0500

    First version of the Profile, with upload button event, two first buttons to edit and calculate BMI (only visually) and creating the component PersonalData to include every input text field to be only shown and not editable yet to the user. Also this first version include media queries to adapt the image profile and buttons

---
+++ src/Profile/PersonalData.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
+
+export default function PersonalData(props) {
+  const classes = useStyles();
+
+
+  return (
+    <form className={classes.container} noValidate autoComplete="off">
+      
+      <TextField
+        id="standard-read-only-input"
+        label="Name"
+        defaultValue={props.name}
+        className={classes.textField}
+        margin="normal"
+        InputProps={{
+          readOnly: true,
+        }}
+      />
+
+      <TextField
+        id="standard-read-only-input"
+        label="Surname"
+        defaultValue={props.surname}
+        className={classes.textField}
+        margin="normal"
+        InputProps={{
+          readOnly: true,
+        }}
+      />
+
+      <TextField
+        id="standard-read-only-input"
+        label="E-mail"
+        defaultValue={props.email}
+        className={classes.textField}
+        margin="normal"
+        InputProps={{
+          readOnly: true,
+        }}
+      />
+
+    <TextField
+        id="standard-read-only-input"
+        label="Height (m)"
+        defaultValue={props.height}
+        className={classes.textField}
+        margin="normal"
+        InputProps={{
+          readOnly: true,
+        }}
+      />
+    <TextField
+        id="standard-read-only-input"
+        label="Weight (kg)"
+        defaultValue={props.weight}
+        className={classes.textField}
+        margin="normal"
+        InputProps={{
+          readOnly: true,
+        }}
+      />
+
+    </form>
+  );
+}
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,17 +1,74 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
+import classes from '../Profile/Profile.css';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import PersonalData from './PersonalData';
 
-class Profile extends Component{
+class Profile extends Component {
 
-    
+    state = {
+        name: 'Andy',
+        surname: 'Newman',
+        email: 'asfsfaf@gmail.com',
+        height: 1.75,
+        weight: 66,
+        avatar: 'https://www.eharmony.com/blog/wp-content/uploads/2010/04/eHarmony-Blog-profile-picture.jpg'
+    }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-            <NavBar />
-            <div>
-            <h1>This is profile</h1>
-            </div>
+                <NavBar />
+                <div>
+                    <h1>Your FitNet Profile</h1>
+                    <div>
+                        <Grid container spacing={3}>
+
+                            <Grid className={classes.gridAvatar} item xs={5}>
+                                <Paper>
+                                    <img className={classes.avatar} src={this.state.avatar} />
+                                    <input
+                                        accept="image/*"
+                                        className={classes.input}
+                                        id="contained-button-file"
+                                        multiple
+                                        type="file"
+                                    />
+                                    <label htmlFor="contained-button-file">
+                                        <Button variant="contained" component="span" className={classes.buttonImage}>
+                                            Upload Profile Image
+                                        </Button>
+                                    </label>
+                                </Paper>
+                            </Grid>
+
+                            <Grid item xs={7}>
+                                <Paper>
+                                    <PersonalData
+                                        name={this.state.name}
+                                        surname={this.state.surname}
+                                        email={this.state.email}
+                                        height={this.state.height}
+                                        weight={this.state.weight} />
+                                </Paper>
+                                <div style={{paddingTop: 7}}>
+                                <Paper className={classes.paperButtons} >
+                                    <Button variant="contained" color="primary" >
+                                        Edit Personal Information
+                                    </Button>
+                                    <div className={classes.buttonSeparator}></div>
+                                    <Button variant="contained" color="primary" >
+                                        Calculate BMI
+                                    </Button>
+                                </Paper>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </div>
+
+                </div>
             </div>
         );
     }
