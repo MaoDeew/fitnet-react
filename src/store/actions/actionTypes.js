b5dd671e2156cd7d7afddcf36edb2ec70af42b3d
MaoDeew <deewweb@gmail.com>
2019-11-04 22:19:52 -0500

    Añadiendo actionTypes y actions al proyecto para usarlos con Redux

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,11 @@
+//ACTION TYPES FOR AUTHENTICATION REDUCER
+export const LOGIN = 'LOGIN';
+export const SIGN_UP = 'SIGN_UP';
+export const LOG_OUT = 'LOG_OUT';
+export const START_LOADING = 'START_LOADING';
+export const END_LOADING = 'END_LOADING';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const RESET_ERROR = 'RESET_ERROR';
+//ACTION TYPES FOR ROUTINES REDUCER
+export const SAVE_ROUTINE = 'SAVE_ROUTINE';
+
