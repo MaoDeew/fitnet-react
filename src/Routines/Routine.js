cc79f5134e47188975dacb34ac1beca5f95f06cc
MaoDeew <deewweb@gmail.com>
2019-11-07 02:20:33 -0500

    Añadiendo imagenes a cada rutina, de manera directa todas las rutinas tienen la misma imagen para pruebas

--- src/Routines/Routine.js
+++ src/Routines/Routine.js
@@ -8,41 +8,52 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      flexBasis: '33.33%',
-      flexShrink: 0,
-    }
-  }));
-  
-  var Routine = (props)=> {
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
-  
-    const handleChange = panel => (event, isExpanded) => {
-      setExpanded(isExpanded ? panel : false);
-    };
-  
-    return (
-      <div className={classesCss.root}>
-        <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
-          >
-            <Typography className={classes.heading}>{props.title}</Typography>
-           
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              {props.description.replace('<p>','').replace('</p>','')}
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        
-      </div>
-    );
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  }, media: {
+    height: 140,
+    maxWidth: '50%',
   }
+}));
 
-  export default Routine;
+var Routine = (props) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const imageRoutine = 'https://cdn.pixabay.com/photo/2017/08/07/14/02/people-2604149_960_720.jpg';
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className={classesCss.root}>
+      <ExpansionPanel  expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>{props.title}</Typography>
+
+        </ExpansionPanelSummary>
+        
+        <ExpansionPanelDetails >
+          
+          <Typography className={classesCss.rezise}>
+            {props.description.replace('<p>', '').replace('</p>', '')}
+          </Typography>
+          <img
+            alt={props.key}
+            className={classes.media}
+            src={imageRoutine}
+          />
+        </ExpansionPanelDetails>
+        
+      </ExpansionPanel>
+
+    </div>
+  );
+}
+
+export default Routine;
