480d65b6216e825ba0dc5d6a1a0751949fb3a5f9
Pachomen <julianalbertop7@gmail.com>
2019-11-10 09:46:28 -0500

    imagen 2

--- src/News/News1.js
+++ src/News/News1.js
@@ -7,6 +7,7 @@ import CardActions from '@material-ui/core/CardActions';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import { ThemeProvider } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme =>({
@@ -24,7 +25,18 @@ const useStyles = makeStyles(theme =>({
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
       width: 200,
-  }
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+
+  img: {
+    margin:'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
   
 }));
 
@@ -40,6 +52,11 @@ var News = (props) => {
         <Typography gutterBottom variant="overline" display = "block">
           21 de Novimebre de 2019
         </Typography>
+        <Grid item>
+          <ButtonBase className={classes.image}>
+            <img className={classes.img} alt="complex" src="1.jpg"/>
+          </ButtonBase>
+        </Grid>
         <Typography variant="body2" color="textSecondary" component="p" justify="left" alignItems="left">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque neque ac augue maximus, ac egestas augue pellentesque. Nullam consequat faucibus orci. Donec ut vulputate ipsum. Ut posuere diam ut odio venenatis dapibus. Ut mattis sapien in enim pharetra sagittis. Fusce vel lectus pretium, auctor ligula ut, efficitur neque. Quisque tincidunt metus nec est molestie, eget interdum felis sodales. Quisque porta nunc tortor. Fusce placerat metus vitae tristique vulputate. Donec tempor facilisis ipsum eget semper. Phasellus vitae mollis nulla, ut volutpat tortor. Pellentesque hendrerit nunc finibus pulvinar aliquam. Vivamus ultricies vehicula orci, quis egestas sapien dignissim nec. Aliquam mollis ante eget tortor placerat interdum. Nunc volutpat arcu bibendum viverra volutpat.
         </Typography>
@@ -51,11 +68,6 @@ var News = (props) => {
         />
         <Grid container direction="row" justify="right" alignItems="right">
         <CardActions>
-        <CardMedia
-          className={classes.media}
-          image="/cloud-computing.png"
-          title={props.title}
-        />
         <Button size="small" className={classes.button}>
           Compartir
         </Button>
