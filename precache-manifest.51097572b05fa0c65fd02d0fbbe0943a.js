b7e18c5264688864f01ed44eb66702066e77c847
MaoDeew <deewweb@gmail.com>
2019-11-14 22:27:27 -0500

    Updates

---
+++ precache-manifest.51097572b05fa0c65fd02d0fbbe0943a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3333a3555ff7da1c2bb04c48ee8a2e0",
+    "url": "/fitnet-react/index.html"
+  },
+  {
+    "revision": "5f3d0ff71a1a38030232",
+    "url": "/fitnet-react/static/css/2.3338ade4.chunk.css"
+  },
+  {
+    "revision": "0cad265c0d504d4b70e9",
+    "url": "/fitnet-react/static/css/main.bbc3373d.chunk.css"
+  },
+  {
+    "revision": "5f3d0ff71a1a38030232",
+    "url": "/fitnet-react/static/js/2.02a087f1.chunk.js"
+  },
+  {
+    "revision": "0cad265c0d504d4b70e9",
+    "url": "/fitnet-react/static/js/main.16691ca3.chunk.js"
+  },
+  {
+    "revision": "fc797fd25798842b7c37",
+    "url": "/fitnet-react/static/js/runtime~main.911f9172.js"
+  }
+]);
